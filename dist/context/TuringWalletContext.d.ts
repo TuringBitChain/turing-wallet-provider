7c4fd5f0b4a63b5c1308d341da0ba1b596d067a0
AlisaMikhaylovna <2322023152@qq.com>
2026-03-23 10:08:42 +0800

    Update TuringWalletContext.d.ts to import TuringProviderType and define TuringContext for improved type safety.

--- dist/context/TuringWalletContext.d.ts
+++ dist/context/TuringWalletContext.d.ts
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { TuringProviderType } from "../types/providerTypes"
+
 export declare const TuringContext: import("react").Context<TuringProviderType | undefined>;
 interface TuringProviderProps {
     children: ReactNode;
